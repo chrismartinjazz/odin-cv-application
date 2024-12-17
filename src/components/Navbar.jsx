a357fbf6b753a3cf425cb7184e8271b853ca11bf
Chris Martin <chrismartinjazz@gmail.com>
2024-12-17 12:22:45 +1030

    Begin css styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
+import '../styles/Navbar.css'
+
 export default function Navbar() {
   const reloadPage = () => {
     window.location.reload()
   }
 
   return (
-    <>
+    <div className="nav">
       <p>CV Builder</p>
       <button onClick={reloadPage}>Reset</button>
-    </>
+    </div>
   )
 }
